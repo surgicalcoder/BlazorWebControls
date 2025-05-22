2573f620fefbcd8ffd7a3d025d0c40c0f5a0e9a2
SurgicalCoder <me@surgicalcoder.com>
2025-05-22 11:39:10 +0100

    IAsyncEnumerable

--- GoLive.Blazor.Controls/PagedResponse.cs
+++ GoLive.Blazor.Controls/PagedResponse.cs
@@ -35,10 +35,10 @@ public class PagedResponse<T> : PagedResponse
         {
             await RunGroupAfterPaging.Invoke(item.Data);
         }
-            
+        
         if (RunEachItemAfterPaging != null)
         {
-            await item.Data.ToAsyncEnumerable().ForEachAwaitAsync(async arg => await RunEachItemAfterPaging(arg));
+            await Task.WhenAll(item.Data.Select(async itemToProcess => await RunEachItemAfterPaging(itemToProcess)));
         }
 
         item.Total = total ?? data.Count();
@@ -47,6 +47,53 @@ public class PagedResponse<T> : PagedResponse
         item.PageCount = Math.Max((item.Total + item.PageSize - 1) / item.PageSize, 1);
 
         return item;
+    }
+
+    public static async Task<PagedResponse<T>> New(IAsyncEnumerable<T> data, int pageIndex, int pageSize, int? total = null, Func<T, Task> RunEachItemAfterPaging = null, Func<List<T>, Task> RunGroupAfterPaging = null)
+    {
+        PagedResponse<T> pagedItem = new();
+        var skipCount = (Math.Max(pageIndex, 1) - 1) * Math.Max(pageSize, 1);
+        var takeCount = Math.Max(pageSize, 1);
+        var pagedData = new List<T>();
+
+        if (pageIndex > 0 && pageSize > 0)
+        {
+            pagedData = await data.Skip(skipCount).Take(takeCount).ToListAsync();
+        }
+        else
+        {
+            pagedData = await data.ToListAsync();
+        }
+
+        pagedItem.Data = pagedData;
+
+        if (RunGroupAfterPaging != null)
+        {
+            await RunGroupAfterPaging.Invoke(pagedItem.Data);
+        }
+
+        if (RunEachItemAfterPaging != null)
+        {
+            await Task.WhenAll(pagedItem.Data.Select(async item => await RunEachItemAfterPaging(item)));
+        }
+
+        int actualTotal;
+
+        if (total.HasValue)
+        {
+            actualTotal = total.Value;
+        }
+        else
+        {
+            actualTotal = await data.CountAsync();
+        }
+
+        pagedItem.Total = actualTotal;
+        pagedItem.Page = Math.Max(pageIndex, 1);
+        pagedItem.PageSize = Math.Max(pageSize, 1);
+        pagedItem.PageCount = Math.Max((pagedItem.Total + pagedItem.PageSize - 1) / pagedItem.PageSize, 1);
+
+        return pagedItem;
     }
 
     public PagedResponse() { }
